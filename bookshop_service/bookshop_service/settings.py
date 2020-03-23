bd7918c965fd51faf07650c933964d6046c5e275
Xavier Martínez <xavier1997@gmail.com>
2020-03-23 11:24:46 +0100

    Renamed database name

--- bookshop_service/bookshop_service/settings.py
+++ bookshop_service/bookshop_service/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'bookshop_service.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'library',
+        'NAME': 'bookstore',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
