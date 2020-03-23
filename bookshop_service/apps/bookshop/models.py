a853a4cda0395a54182b36ae669f4e43f6e6405d
Xavier Martínez <xavier1997@gmail.com>
2020-03-23 11:07:46 +0100

    Add bookstore model and migrations

--- bookshop_service/apps/bookshop/models.py
+++ bookshop_service/apps/bookshop/models.py
@@ -1,3 +1,41 @@
+from django.db.models import PROTECT, SET_NULL, CASCADE
 from django.db import models
 
-# Create your models here.
+# usuarios(id,nombre,edad,fecha_registro,telefono,puntos)
+# compras:(id,usuario,libro)
+# libros(id,titulo,genero,cantidad,precio,fecha_registro)
+# generos(id_genero,nombre)
+# stock(id_cantidad,cantidad)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    birth_date = models.DateField(null=True)
+    register_date = models.DateTimeField(auto_now_add=True)
+    phone = models.CharField(max_length=15)
+    fidelity_points = models.IntegerField(default=0)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    genre = models.ForeignKey(Genre, SET_NULL, null=True)
+    available_stock = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, PROTECT)
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    purchased_books = models.ManyToManyField(Book, through='OrderBook')
+
+
+class OrderBook(models.Model):
+    order = models.ForeignKey(Order, CASCADE)
+    book = models.ForeignKey(Book, CASCADE)
+    quantity = models.IntegerField()
