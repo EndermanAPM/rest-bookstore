b952f1c56fef2d4f48127a693b58523efbd18565
Xavier Martínez <xavier1997@gmail.com>
2020-03-24 10:03:13 +0100

    Removes nested post possibility on orderbooks and instead lists them as readonly on orderbook summary

--- bookshop_service/apps/bookshop/serializers.py
+++ bookshop_service/apps/bookshop/serializers.py
@@ -30,31 +30,30 @@ class OrderBookQuantitySerializer(HyperlinkedModelSerializerWithId):
     class Meta:
         model = OrderBookQuantity
         fields = "__all__"
-        # exclude = ("order", )
-
-class OrderlessOrderBookQuantitySerializer(HyperlinkedModelSerializerWithId):
-    class Meta:
-        model = OrderBookQuantity
-        exclude = ("order", )
 
 class OrderSerializer(HyperlinkedModelSerializerWithId):
-    purchased_books = OrderlessOrderBookQuantitySerializer(many=True)
-
     class Meta:
         model = Order
         fields = "__all__"
 
     @atomic
     def create(self, validated_data):
-        validated_purchased_books = validated_data.pop('purchased_books')
+        super()
         order = Order.objects.create(**validated_data)
-        book_serializer = self.fields['purchased_books']
-        for each in validated_purchased_books:
-            each['order'] = order
-        choices = book_serializer.create(validated_purchased_books)
-
         customer = Customer.objects.get(pk=order.customer_id)
         customer.fidelity_points += 1
         customer.save()
         return order
 
+class OrderDetailSerializer(HyperlinkedModelSerializerWithId):
+
+    class OrderlessOrderBookQuantitySerializer(HyperlinkedModelSerializerWithId):
+        class Meta:
+            model = OrderBookQuantity
+            exclude = ("order",)
+
+    purchased_books = OrderlessOrderBookQuantitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = "__all__"
